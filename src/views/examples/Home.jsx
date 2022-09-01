3bf6668764732292811cd3672e01c62c415cd27d
Deriki Pereira <89177022+derikipereira@users.noreply.github.com>
2022-09-01 17:06:14 -0300

    v0.1

---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Home = props => (
+    <div className="Home">
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+    </div>
+)
+
+export default Home
